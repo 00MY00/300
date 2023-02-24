e1b95fb03963ee73a7fbbbc00f8428a31ad06e6e
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-02-24 10:57:54 +0100

    Add files via upload

---
+++ DNS_Teampass.ps1
@@ -0,0 +1,71 @@
+﻿# IP
+For ([int]$i = 0;$i -lt 1;)
+{
+    Write-Host "`nEntez l'IP v4 de la machine TeamPass !"
+    Write-Host "Exemple : 192.168.0.10"
+    [string]$IP = Read-Host "► "
+
+    if ($IP -as [ipaddress]) {
+        Write-Host "L'adresse IP est valide." -ForegroundColor Green
+        break
+    }
+    else {
+        Write-Host "L'adresse IP n'est pas valide." -ForegroundColor Red
+    }
+
+
+}
+
+# Name
+For ([int]$i = 0;$i -lt 1;)
+{
+    Write-Host "`nEntez le nom du DNS !"
+    Write-Host "Exemple : teampass"
+    [string]$Name = Read-Host "► "
+
+}
+
+
+# ZoneName
+For ([int]$i = 0;$i -lt 1;)
+{
+    Write-Host "`nEntez le nom de zone !"
+    Write-Host "Exemple : local"
+    [string]$ZoneName = Read-Host "► "
+
+}
+
+# Recapitulatif
+For ([int]$i = 0;$i -lt 1;)
+{
+    clear
+    Write-Host "`n"
+    Write-Host "Récapitulatife de l'ajout d'adress DNS" -ForegroundColor Yellow
+    Write-Host "---------------"
+    Write-Host "IP               - $IP" -ForegroundColor DarkCyan
+    Write-Host "Name             - $Name" -ForegroundColor DarkCyan
+    Write-Host "ZoneName         - $ZoneName" -ForegroundColor DarkCyan
+    Write-Host "TTL              - 01:00:00" -ForegroundColor Magenta
+    Write-Host "`n"
+    Write-Host "[OK] pour continuer !"
+    [string]$Validation = Read-Host "► "
+    $Validation = $Validation.ToUpper()
+    if ($Validation -eq "OK") {
+        Write-Host "[OK] `n" -ForegroundColor Green
+        break
+    }
+    else {
+        exit
+    }
+}
+
+# Commande Final
+Add-DnsServerResourceRecordA -Name "$Name" -ZoneName "$ZoneName" -IPv4Address "$IP" -TimeToLive 01:00:00
+
+
+
+
+
+
+
+
