64d51b87a9e38a3e67e12501b028220199a5937a
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-03-08 16:27:58 +0100

    Add files via upload

--- Secure_TeamPass_With_SSL.sh
+++ Secure_TeamPass_With_SSL.sh
@@ -154,14 +154,95 @@ a2enmod ssl
 # Potentielement inutile 
 # ln -s /etc/apache2/sites-available/teampass.domaintest.loc.conf /etc/apache2/sites-enabled/teampass.domaintest.loc.conf
 
+00=0
+P53=0
+P88=0
+P3269=0
+P3268=0
+P636=0
+P389=0
+P443=0
+P22=0
+
+
+while true; do
+    clear
+    echo -e "Sélectionnez les Ports a ouvrire \033[32m[]\033[00m Ouvert \033[31m[]\033[00m Fermer"
+    echo -e "Port conseiler 53, 336, 443"
+    echo -e "OUVERTURE de Port"
+    if [[ $P53 -eq 1 ]]; then echo -e "\33[32m[1] \033[00m   Port : 53   | DNS"; else echo -e "\33[31m[1] \033[00m   Port : 53   | DNS"; fi
+    if [[ $P88 -eq 1 ]]; then echo -e "\33[32m[2] \033[00m   Port : 88   | Kerberos"; else echo -e "\33[31m[2] \033[00m   Port : 88   | Kerberos"; fi
+    if [[ $P3269 -eq 1 ]]; then echo -e "\33[32m[3] \033[00m   Port : 3269 | LDAPS Catalog"; else echo -e "\33[31m[3] \033[00m   Port : 3269 | LDAPS Catalog"; fi
+    if [[ $P3268 -eq 1 ]]; then echo -e "\33[32m[4] \033[00m   Port : 3268 | LDAP Catalog"; else echo -e "\33[31m[4] \033[00m   Port : 3268 | LDAP Catalog"; fi
+    if [[ $P636 -eq 1 ]]; then echo -e "\33[32m[5] \033[00m   Port : 636  | LDAPS Secure"; else echo -e "\33[31m[5] \033[00m   Port : 636  | LDAPS Secure"; fi
+    if [[ $P389 -eq 1 ]]; then echo -e "\33[32m[6] \033[00m   Port : 389  | LDAP  Non chiffrée"; else echo -e "\33[31m[6] \033[00m   Port : 389  | LDAP  Non chiffrée"; fi
+    if [[ $P443 -eq 1 ]]; then echo -e "\33[32m[7] \033[00m   Port : 443  | HTTPS"; else echo -e "\33[31m[7] \033[00m   Port : 443  | HTTPS"; fi
+    if [[ $P443 -eq 1 ]]; then echo -e "\33[32m[8] \033[00m   Port : 22  | SSH"; else echo -e "\33[31m[8] \033[00m   Port : 22  | SSH"; fi
+    echo -e "[9] EXIT"
+    echo ""
+    echo ""
+    read -p "Entrez le numéro du port que vous souhaitez ajouter : " port_number
+
+    case $port_number in
+        1) 
+            iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
+            P53=1
+            Ports[0]="53"
+            ;;
+        2)
+            iptables -A INPUT -p tcp -m tcp --dport 88 -j ACCEPT
+            P88=1
+            Ports[1]="88"
+            ;;
+        3)
+            iptables -A INPUT -p tcp -m tcp --dport 3269 -j ACCEPT
+            P3269=1
+            Ports[2]="3269"
+            ;;
+        4)
+            iptables -A INPUT -p tcp -m tcp --dport 3268 -j ACCEPT
+            P3268=1
+            Ports[3]="3268"
+            ;;
+        5)
+            iptables -A INPUT -p tcp -m tcp --dport 636 -j ACCEPT
+            P636=1
+            Ports[4]="636"
+            ;;
+        6)
+            iptables -A INPUT -p tcp -m tcp --dport 389 -j ACCEPT
+            P389=1
+            Ports[5]="389"
+            ;;
+        7)
+            iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+            P443=1
+            Ports[6]="443"
+            ;;
+        8)
+            iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+            P22=1
+            Ports[7]="22"
+            ;;
+        9)
+            break
+            ;;
+        *)
+            echo "Numéro de port invalide, veuillez réessayer."
+            ;;
+    esac
+done
+
+
+
 systemctl restart apache2
 
 apt-get install iptables-persistent -y
 
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+#iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+#iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+#iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -P INPUT DROP
 
@@ -174,6 +255,6 @@ echo "" >> $back/Raport.txt
 
 echo "" >> $back/Raport.txt
 echo "Configuration SSL pour HTTPS" >> $back/Raport.txt
-echo "Port, 22, 80, 443 Ouvert sur la machine" >> $back/Raport.txt
+echo -e "Port, ${Ports[0]} ${Ports[1]} ${Ports[2]} ${Ports[3]} ${Ports[4]} ${Ports[5]} ${Ports[6]} ${Ports[7]}  Ouvert sur la machine" >> $back/Raport.txt
 echo "Pour acéder a TeamPass HTTPS 'https://localhost' ou 'https://$localhostIP'" >> $back/Raport.txt
 echo "" >> $back/Raport.txt
